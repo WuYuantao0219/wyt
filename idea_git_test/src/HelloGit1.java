4d43caf670eb143117958f68a5e20b207c0d23ca
wyt <xmwxs8@126.com>
2019-06-15 20:21:19 +0800

    添加test工程至git

--- idea_git_test/src/HelloGit1.java
+++ idea_git_test/src/HelloGit1.java
@@ -2,7 +2,7 @@
  * @author 吴元陶
  * @date 2019/6/15 20:00
  */
-public class HelloGit {
+public class HelloGit1 {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello git!");
